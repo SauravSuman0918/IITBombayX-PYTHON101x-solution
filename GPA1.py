3a17efbeaba432488c52a02ba779a2eb7f09bfd3
SauravSuman0918 <71310624+SauravSuman0918@users.noreply.github.com>
2021-04-05 15:14:11 +0530

    Create GPA1.py

---
+++ GPA1.py
@@ -0,0 +1,4 @@
+# Write a Function called num_square to accept an integer as parameter and return the square of that number.
+# Define your function called `num_square` below
+def num_square(n):
+    return n*n
