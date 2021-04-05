5d860de21bb7ce3bdbd9112b9435c77eee6c11aa
SauravSuman0918 <71310624+SauravSuman0918@users.noreply.github.com>
2021-04-05 15:15:37 +0530

    Create GPA2.py

---
+++ GPA2.py
@@ -0,0 +1,6 @@
+# Write a function except_last_two which takes a list of integers 'L'as an argument and returns everything except the last two elements of the list L.
+# Define your function called `except_last_two` below
+
+def except_last_two(L):
+    print([23,54,78,24,97,35,66,39][:-2]==[23,54,78,24,97,35])
+    return L[:-2]
